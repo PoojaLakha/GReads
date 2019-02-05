aba73ec360ff6439cd2cd11d7acd4c6de4ebf25a
PoojaLakha <pooja_nl@outlook.com>
2019-02-05 14:33:34 +0530

    wrong commit

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'users'
-
-urlpatterns = [
-    path('profile/', views.view_profile, name='view_profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-]
