0be27c88925628ec70aa7863d00947203ce8bb70
PoojaLakha <pooja_nl@outlook.com>
2019-02-14 09:42:30 +0530

    logout modified

--- greads/accounts/urls.py
+++ greads/accounts/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
