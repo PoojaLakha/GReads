0a831ed6a8884f4f4adb9f83e168b44006c23f8c
PoojaLakha <pooja_nl@outlook.com>
2019-02-26 19:30:44 +0530

    UserBook table added

--- greads/books/admin.py
+++ greads/books/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from books.models import Book, Author, Genre
+from books.models import Book, Author, Genre, UserBook
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
+admin.site.register(UserBook)
--- greads/books/models.py
+++ greads/books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import User
+import uuid
 
 
 # Create your models here.
@@ -17,7 +18,7 @@ class Author(models.Model):
     # website = models.CharField(blank=True)
     # twitter_id = models.CharField(blank=True)
     # member_since = models.DateField()
-    # about = models.TextField()
+    about = models.TextField(max_length=2000, null=True)
 
     def __str__(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
@@ -36,11 +37,32 @@ class Book(models.Model):
     # no_of_pages = models.IntegerField()
 
     def __str__(self):
-        return '%s %s' % (self.title, self.author)
+        return self.title
 
 
-'''
-class BookAuthor(models.Model):
-    book_id = models.Foreignkey('Book', on_delete=models.CASCADE)
-    author_id = models.Foreignkey('Author', on_delete=models.CASCADE)
-'''
+class UserBook(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    reader = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               null=True)
+
+    BOOK_SHELF = (
+        ('n', 'none'),
+        ('c', 'Currently reading'),
+        ('r', 'Read'),
+        ('w', 'want to read'),
+    )
+
+    shelf = models.CharField(
+        max_length=1,
+        choices=BOOK_SHELF,
+        blank=False,
+        default='n',
+        help_text='Book shelf',
+    )
+
+    class Meta:
+        unique_together = (('book', 'reader'),)
+
+    def __str__(self):
+        return '%s %s' % (self.id, self.book.title)
